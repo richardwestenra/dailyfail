9913323af81c80ea6f93df1f0616c0fbf49e0f5c
Richard Westenra <rjwestenra@gmail.com>
2014-03-18 01:21:28 +0000

    add DM classes etc.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,21 @@
 "use strict";
-
 $(function(){
-	$('.foo').each(function(){
+	var headlines = [
+		'.article .linkro-darkred a',
+		'.link-bogr2 li a'
+	],
+	commentClass = '.comment-text',
+	maxLength = 140;
+	$( headlines.join(', ') ).each(function(){
 		var url = $(this).attr('href'),
 			$this = $(this);
 		$.get(url, function(data) {
-			console.log("Success! Data received from "+url);
-			var comment = $(data).find('.bar').text();
+			var comment = $(data).find(commentClass+':first').text();
+			if (typeof comment === "string" && comment.length>maxLength) {
+				comment = comment.substr(0,maxLength) + "…";
+			}
+			console.log("Success! Data received from "+url+". Comment reads: '"+comment+"'.");
 			$this.text(comment);
-		});
+		});	
 	});
 });
