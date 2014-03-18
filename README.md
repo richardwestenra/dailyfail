9f73b4bead5c68e3707d11f65103296af4625ff3
Richard Westenra <rjwestenra@gmail.com>
2014-03-18 00:51:11 +0000

    HTML parsing initial demo

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ dailyfail
 Bookmarklet to replace the headlines of Daily Mail articles with their comments.
 
 
-Based on an idea by @TechnicallRon: https://twitter.com/TechnicallyRon/statuses/445178115887595520
+Based on an idea by @TechnicallyRon: https://twitter.com/TechnicallyRon/statuses/445178115887595520
 
 
 TBC...
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+"use strict";
+
+$(function(){
+	$('.foo').each(function(){
+		var url = $(this).attr('href'),
+			$this = $(this);
+		$.get(url, function(data) {
+			console.log("Success! Data received from "+url);
+			var comment = $(data).find('.bar').text();
+			$this.text(comment);
+		});
+	});
+});
