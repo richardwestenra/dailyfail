b566023910917313bb675cf55b63a9ae115bc8be
Richard Westenra <rjwestenra@gmail.com>
2014-11-03 09:59:45 +0000

    Fix JSHint errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
-"use strict";
+/* jshint camelcase:false */
 $(function(){
+	'use strict';
 	var headlines = [
 		'.article .linkro-darkred',
 		'editors-choice li',
@@ -10,14 +11,14 @@ $(function(){
 	maxLength = 200;
 	function truncate(s,n,useWordBoundary){
 		var toLong = s.length>n,
-			s_ = toLong ? s.substr(0,n-1) : this;
+			s_ = toLong ? s.substr(0,n-1) : s;
 		s_ = useWordBoundary && toLong ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
 		return  toLong ? s_ + '…' : s_;
-	};
+	}
 	var ajaxCalls = [];
 	var loadingMessage = $('<p/>',{
-				'class':'loadingMessage'
-		}).html("Loading comments&hellip;")
+			'class':'loadingMessage'
+		}).html('Loading comments&hellip;')
 		.css({
 			'position':'fixed',
 			'top': '10px',
@@ -32,21 +33,21 @@ $(function(){
 			'font-family': '"Helvetica Neue", Helvetica, Arial, sans-serif'
 		});
 	$(headlines).each(function(key,val){
-		$(val).find('a').each(function(key2,val2){
+		$(val).find('a').each(function(key2){
 			loadingMessage.appendTo('body');
 			var $this = $(this),
 				url = $this.attr('href'),
 				uid = key+'-'+key2;
-				ajaxCalls.push(uid);
+			ajaxCalls.push(uid);
 			var get = $.get(url, function(data) {
 				var comment = $(data).find(commentClass+':first').text();
-				if (typeof comment != 'string' || comment.length === 0) {
+				if (typeof comment !== 'string' || comment.length === 0) {
 					comment = 'No comment';
 				} else if(comment.length>maxLength) {
 					comment = truncate(comment,maxLength,true);
 				}
 				// console.log("Success! Data received from "+url+". Comment reads: '"+comment+"'.");
-				if(val == '.linkro-wocc li'){
+				if(val === '.linkro-wocc li'){
 					$this.find('strong').text(comment);
 				} else {
 					$this.text(comment);
@@ -54,7 +55,7 @@ $(function(){
 			});
 			get.always(function(){
 				var i = ajaxCalls.indexOf(uid);
-				if (i != -1) {
+				if (i !== -1) {
 					ajaxCalls.splice(i, 1);
 				}
 				if (ajaxCalls.length < 1){
